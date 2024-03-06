ceea24aed45a25b8d0ee2a31dbe10a42841b79e1
Nael Reinhard <nael.reinhard@swenex.ch>
2024-03-06 09:39:36 +0100

    Merge remote-tracking branch 'origin/main'

--- M335-EFN/src/app/sjtab/sjtab.page.ts
+++ M335-EFN/src/app/sjtab/sjtab.page.ts
@@ -15,12 +15,34 @@ export class sjtabPage {
 
   async presentAlert() {
     const alert = await this.alertController.create({
-      header: 'Bestätigung',
-      message: 'Das hat geklappt',
-      buttons: ['OK']
+      header: 'Gib deinen Namen ein',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Dein Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Abbrechen',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Bestätigung abgebrochen');
+          }
+        }, {
+          text: 'Weiter',
+          handler: (data) => {
+            console.log('Bestätigung weiter mit Name:', data.name);
+          }
+        }
+      ]
     });
+
     await alert.present();
   }
+
   startHunt() {
     this.presentAlert();
   }
