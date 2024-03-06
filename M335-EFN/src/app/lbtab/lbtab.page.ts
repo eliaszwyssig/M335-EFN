ce363ca9c1fcb5ab5d9e2809ed2ac5c44aca55ea
Elias Zwyssig <elias_zwyssig@sluz.ch>
2024-03-06 13:55:53 +0100

    add data with mock

--- M335-EFN/src/app/lbtab/lbtab.page.ts
+++ M335-EFN/src/app/lbtab/lbtab.page.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonLabel,
+  IonItem,
+  IonImg
+} from '@ionic/angular/standalone';
+import {LEADERBOARDMOCK} from "../mock-leaderboard";
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-lbtab',
   templateUrl: 'lbtab.page.html',
   styleUrls: ['lbtab.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgFor, IonList, IonLabel, IonItem, IonImg]
 })
-export class lbtabPage {
+export class lbtabPage{
+  leaderboards = LEADERBOARDMOCK;
 
-  constructor() {}
+
+  constructor() {
+    this.sortLeaderboardsByTime();
+  }
+
+  sortLeaderboardsByTime() {
+    this.leaderboards.sort((a, b) => {
+      const timeA = this.convertTimeToSeconds(a.time);
+      const timeB = this.convertTimeToSeconds(b.time);
+      return timeA - timeB;
+    });
+  }
+
+  convertTimeToSeconds(timeString: string): number {
+    const [hours, minutes] = timeString.split(':').map(Number);
+    return hours * 3600 + minutes * 60 ;
+  }
+
+
+
+
 }
 
