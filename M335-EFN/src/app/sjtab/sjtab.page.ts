79b8b038f6ab0ac53abdb3ad06d3d568c447aa86
Nael Reinhard <nael.reinhard@swenex.ch>
2024-03-06 14:19:10 +0100

    Gib deinen Namen ein FIXED

--- M335-EFN/src/app/sjtab/sjtab.page.ts
+++ M335-EFN/src/app/sjtab/sjtab.page.ts
@@ -1,63 +1,52 @@
 import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonInput,
+  IonLabel,
+  IonItem
+} from '@ionic/angular/standalone';
 import { AlertController } from '@ionic/angular';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-sjtab',
   templateUrl: 'sjtab.page.html',
   styleUrls: ['sjtab.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, FormsModule, IonInput, IonLabel, IonItem, NgIf],
 })
 
 export class sjtabPage {
+  showNameInput = false;
+  playerName: string = '';
+
   constructor(public alertController: AlertController,
               private router: Router) { }
 
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      header: 'Gib deinen Namen ein',
-      inputs: [
-        {
-          name: 'name',
-          type: 'text',
-          placeholder: 'Dein Name'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Abbrechen',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('Bestätigung abgebrochen');
-          }
-        }, {
-          text: 'Weiter',
-          handler: async (data) => {
-            if (data.name.trim() === '') {
-              const errorAlert = await this.alertController.create({
-                header: 'Fehler',
-                message: 'Bitte gib einen Namen ein!',
-                buttons: ['OK']
-              });
-              await errorAlert.present();
-              return false;
-            } else {
-              this.router.navigateByUrl('/permissions');
-              return true;
-            }
-          }
-        }
-      ]
-    });
-
-    await alert.present();
+  toggleNameInput(show: boolean) {
+    this.showNameInput = show;
+    if (!show) {
+      this.playerName = '';
+    }
   }
 
-  getPermissions() {
-    this.presentAlert();
+  async submitName() {
+    if (this.playerName.trim() === '') {
+      const errorAlert = await this.alertController.create({
+        header: 'Fehler',
+        message: 'Bitte gib einen Namen ein!',
+        buttons: ['OK']
+      });
+      await errorAlert.present();
+    } else {
+      this.router.navigateByUrl('/permissions');
+    }
   }
 
 }
