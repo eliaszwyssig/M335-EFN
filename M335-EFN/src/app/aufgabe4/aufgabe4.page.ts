3c75fa084191fa31217ca31c8ed1508a8dc46c8b
Fatmir Margegaj <fatmir.margegaj@getabstract.com>
2024-03-11 10:41:08 +0100

    aufgabe 4

--- M335-EFN/src/app/aufgabe4/aufgabe4.page.ts
+++ M335-EFN/src/app/aufgabe4/aufgabe4.page.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
-import {Router} from "@angular/router";
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Device } from "@capacitor/device";
+import { IonicModule } from "@ionic/angular";
+import {NgIf} from "@angular/common";
+import {batteryDead, batteryCharging} from "ionicons/icons";
+import {addIcons} from "ionicons";
 
 @Component({
   selector: 'app-aufgabe4',
   templateUrl: './aufgabe4.page.html',
   styleUrls: ['./aufgabe4.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, NgIf]
 })
-export class Aufgabe4Page {
+export class Aufgabe4Page implements OnDestroy {
+  isCharging: boolean = false;
+  intervalId: any;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.startChargingStatusCheck();
+    addIcons({batteryCharging,batteryDead })
+  }
 
+  async goToExercise6() {
+    this.router.navigateByUrl('/aufgabe5');
+  }
 
-  async goToExercise5(){
-    this.router.navigateByUrl("/aufgabe5");
+  startChargingStatusCheck() {
+    this.checkChargingStatus();
+    this.intervalId = setInterval(() => {
+      this.checkChargingStatus();
+    }, 1);
+  }
+
+  async checkChargingStatus() {
+    const info = await Device.getBatteryInfo();
+    this.isCharging = info.isCharging ?? false;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
   }
 }
